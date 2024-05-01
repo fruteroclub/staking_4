6625a309bb93ac56612216233cd2bd9199855d0c
ArturVargas <arturvargas9226@gmail.com>
2024-05-01 16:41:09 -0600

    update next config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    loader: 'akamai',
+    path: '',
+  },
+  trailingSlash: true,
+  assetPrefix: isProd ? 'staking-4-6c3cfa.spheron.app' : '',
 };
 
 module.exports = nextConfig;
