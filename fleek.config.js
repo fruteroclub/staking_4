497dd48f339f85b8977d62654d550be76ae22a81
ArturVargas <arturvargas9226@gmail.com>
2024-05-06 09:55:49 -0600

    add fleek config

---
+++ fleek.config.js
@@ -0,0 +1,10 @@
+/** @type {import('@fleekxyz/cli').FleekConfig} */
+module.exports = {
+  "sites": [
+    {
+      "slug": "hundreds-thailand-rapid",
+      "distDir": "out",
+      "buildCommand": "npm run build"
+    }
+  ]
+};
